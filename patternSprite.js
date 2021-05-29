39f4d0a92cc96b87e0419f1c85a7562081e06120
BossRpg <45954325+BossRpg@users.noreply.github.com>
2021-05-29 02:16:31 -0300

    Update patternSprite.js

--- patternSprite.js
+++ patternSprite.js
@@ -3,32 +3,40 @@
 *@plugindesc Define any pose of a sprite
 
 *@help Here's the command:
-spritePattern(Event ID, pattern, direction)
+spritePattern(Event ID, Pose)
 
 Event ID: this or number
 
-pattern: 0, 1, 2
-   
-Directions:
-   Down = 2
-   Left = 4
-   Right = 6
-   Up = 8
+Pose: 1..9
    
 Example (Event current):
-   spritePattern(this, 0, 2)
+   spritePattern(this, 1)
 */
 
-function spritePattern(t,p,d){
-   /* Check number */
-   var ev;
-   if(typeof(t) == "number"){
-      ev=t;
-   }
-   if(typeof(t) == "object"){
-      ev=t._eventId;
-   }
-   $gameMap._events[ev]._pattern=p;
-   $gameMap._events[ev]._originalPattern=p;
-   $gameMap._events[ev]._direction=d;
+function spritePattern(t,p){
+	/* Check number */
+	var ev, pose, dir;
+	if(typeof(t) == "number"){
+		ev=t;
+	}
+	if(typeof(t) == "object"){
+		ev=t._eventId;
+	}
+
+	if(p==1){pose=0;dir=2;}
+	if(p==2){pose=1;dir=2;}
+	if(p==3){pose=2;dir=2;}
+	if(p==4){pose=0;dir=4;}
+	if(p==5){pose=1;dir=4;}
+	if(p==6){pose=2;dir=4;}
+	if(p==7){pose=0;dir=6;}
+	if(p==8){pose=1;dir=6;}
+	if(p==9){pose=2;dir=6;}
+	if(p==10){pose=0;dir=8;}
+	if(p==11){pose=1;dir=8;}
+	if(p==12){pose=2;dir=8;}
+
+	$gameMap._events[ev]._pattern=pose;
+	$gameMap._events[ev]._originalPattern=pose;
+	$gameMap._events[ev]._direction=dir;
 }
